1f0bdad55d96fb473ea68f46c2a015d46ef60bd3
hayden <1haydengragnano@gmail.com>
2021-11-10 20:15:52 -0800

    adds better data

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :handle, :email, :image_url, :stream_url, :bio
+  attributes :id, :handle, :email, :image_url, :stream_url, :bio, :game_id
 
   belongs_to :game
   has_many :tags
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,16 @@
 # Game.create!([
-#   {title: "Halo Infinite", image_url: "https://static-cdn.jtvnw.net/ttv-boxart/Halo%20Infinite-144x192.jpg"},
+#   {title: "Halo Infinite", image_url: "https://static-cdn.jtvnw.net/ttv-boxart/Halo%20Infinite-285x380.jpg"},
 #   {title: "Call of Duty: Warzone", image_url: "https://static-cdn.jtvnw.net/ttv-boxart/Call%20of%20Duty:%20Warzone-285x380.jpg"},
 #   {title: "Grand Theft Auto V", image_url: "https://static-cdn.jtvnw.net/ttv-boxart/Grand%20Theft%20Auto%20V-285x380.jpg"},
 #   {title: "VALORANT", image_url: "https://static-cdn.jtvnw.net/ttv-boxart/VALORANT-285x380.jpg"},
-#   {title: "Hell Let Loose", image_url: "https://static-cdn.jtvnw.net/ttv-boxart/Hell%20Let%20Loose-144x192.jpg"},
-#   {title: "Apex Legends", image_url: "https://static-cdn.jtvnw.net/ttv-boxart/Apex%20Legends-144x192.jpg"},
+#   {title: "Hell Let Loose", image_url: "https://static-cdn.jtvnw.net/ttv-boxart/Hell%20Let%20Loose-285x380.jpg"},
+#   {title: "Apex Legends", image_url: "https://static-cdn.jtvnw.net/ttv-boxart/Apex%20Legends-285x380.jpg"},
 #   {title: "Rocket League", image_url: "https://static-cdn.jtvnw.net/ttv-boxart/Rocket%20League-285x380.jpg"},
 #   {title: "New World", image_url: "https://static-cdn.jtvnw.net/ttv-boxart/New%20World-285x380.jpg"},
 #   {title: "Fortnite", image_url: "https://static-cdn.jtvnw.net/ttv-boxart/Fortnite-285x380.jpg"},
-#   {title: "League of Legends", image_url: "https://static-cdn.jtvnw.net/ttv-boxart/League%20of%20Legends-144x192.jpg"},
-#   {title: "World of Warcraft", image_url: "https://static-cdn.jtvnw.net/ttv-boxart/World%20of%20Warcraft-144x192.jpg"},
-#   {title: "Battlefield 2042", image_url: "https://static-cdn.jtvnw.net/ttv-boxart/Battlefield%202042-144x192.jpg"}
+#   {title: "League of Legends", image_url: "https://static-cdn.jtvnw.net/ttv-boxart/League%20of%20Legends-285x380.jpg"},
+#   {title: "World of Warcraft", image_url: "https://static-cdn.jtvnw.net/ttv-boxart/World%20of%20Warcraft-285x380.jpg"},
+#   {title: "Battlefield 2042", image_url: "https://static-cdn.jtvnw.net/ttv-boxart/Battlefield%202042-285x380.jpg"}
 # ])
 
 # Tag.create!([
